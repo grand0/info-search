9cd31d2f27d2d5171aa81714541a8b13c752463d
dmitry <dmitparam.2017@gmail.com>
2026-02-18 18:45:40 +0300

    README.md, code comments

--- read_articles.py
+++ read_articles.py
@@ -6,25 +6,39 @@ urls_path = "urls.txt"
 index_path = "index.txt"
 
 def read_urls():
+	"""
+	Returns article URLs from urls.txt file
+	"""
 	with open(urls_path, "r") as file:
 		urls = file.read().splitlines()
 	return urls
 
 def read_url(url):
+	"""
+	Reads article content using newspaper3k library from given URL
+	"""
 	article = Article(url, keep_article_html=True, language="ru")
 	article.download()
 	article.parse()
+	# unescaping cyrillic HTML characters for readability
 	return html.unescape(article.article_html)
 
 def write_article(out_path, content):
+	"""
+	Writes article content into given file path
+	"""
 	with open(out_path, "w") as file:
 		file.write(content)
 
-def main():
+def read_articles():
+	"""
+	Reads article URLs from urls.txt file, gets article content from each URL and writes content in separate files
+	"""
 	index = 1
 	urls = read_urls()
 	os.makedirs("articles", exist_ok = True)
-	with open(index_path, "a") as index_file:
+	with open(index_path, "w") as index_file:
+		# iterating over urls, getting its content and writing into corresponding files
 		for url in urls:
 			html = read_url(url)
 			out_path = os.path.join("articles", f"{index}.txt")
@@ -34,4 +48,4 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+	read_articles()
